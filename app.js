5d22afeba4141ce4e87490535172c5a00b1f2fe1
KevinLiTian <kevin.li20021106@gmail.com>
2022-07-25 00:43:19 -0400

    Grid

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const box_container = document.querySelector('.box-container');
-    const box = box_container.children[0];
 
-    // Play Animation
-    box_container.addEventListener('click', () => {
-        box.style.animationPlayState = 'running';
+    tableCreate();
+    document.querySelectorAll('.box-container').forEach((box_container) => {
+        const box = box_container.children[0];
+        // Play Animation
+        box_container.addEventListener('click', () => {
+            box.style.animationPlayState = 'running';
+        });
+
+        // Set Animation
+        box.addEventListener('animationend', () => setBoxAnimationState(box));
     });
+});
 
-    // Set Animation
-    box.addEventListener('animationend', () => setBoxAnimationState(box));
-})
+// var rect = box_container.getBoundingClientRect();
+// console.log(rect.top, rect.left, rect.right, rect.bottom);
+// event.clientX, event.clientY
 
 /* Stop Animation and Set Animation Accordingly */
 function setBoxAnimationState(box) {
@@ -26,4 +32,28 @@ function setBoxAnimationState(box) {
         box.classList.add('stretch');
         box.dataset.animation = 'stretch';
     }
+}
+
+/* Create Grid */
+function tableCreate() {
+    const grid = document.querySelector('#grid');
+    const tbl = document.createElement('table');
+    tbl.style.borderCollapse = 'collapse';
+    tbl.style.width = '1250px';
+
+    for (let i = 0; i < 20; i++) {
+        const tr = tbl.insertRow();
+        for (let j = 0; j < 50; j++) {
+            const td = tr.insertCell();
+            td.classList.add('box-container', 'inline-float-l');
+
+            // Box
+            const box = document.createElement('div');
+            box.classList.add('box', 'stretch');
+            box.dataset.animation = 'stretch';
+
+            td.append(box);
+        }
+    }
+    grid.append(tbl);
 }
