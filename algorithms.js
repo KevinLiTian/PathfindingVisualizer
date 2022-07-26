df8fda5ca1befe7bb3dcdf88097d346f407db385
KevinLiTian <kevin.li20021106@gmail.com>
2022-07-26 12:58:50 -0400

    Revert "Complete DFS"
    This reverts commit df1ae9a4b49f993ca06a3bd512c468a64c4142c0.

---
+++ algorithms.js
@@ -0,0 +1,110 @@
+/* Algorithms */
+
+import { SRC, DEST, neighbors } from './app.js';
+
+// Returns a Promise that resolves after "ms" Milliseconds
+const timer = ms => new Promise(res => setTimeout(res, ms));
+
+class Node {
+    constructor(state, parent) {
+        this.state = state;
+        this.parent = parent;
+    }
+}
+
+async function dfs() {
+
+    // Store all nodes to be traversed
+    let stackFrontier = [];
+    stackFrontier.push(new Node(SRC, null));
+
+    // Store all searched nodes to prevent infinite search
+    let searched = [];
+    searched.push(SRC);
+
+    // Traverse until no node can be found
+    while (stackFrontier.length != 0) {
+
+        // Search the next node
+        let curNode = stackFrontier.pop();
+
+        // Check if current node is the destination
+        if (JSON.stringify(curNode.state) === JSON.stringify(DEST)) {
+            return backTrack(curNode);
+        }
+
+        animateSearch(curNode.state);
+
+        // Push all the neighbors of current node
+        neighbors[curNode.state].forEach(idx => {
+            if (!exists(searched, idx)) {
+                stackFrontier.push(new Node(idx, curNode));
+                searched.push(idx);
+            }
+        })
+        await timer(100);
+    }
+
+    return null;
+}
+
+function bfs() {
+
+}
+
+function greedy() {
+
+}
+
+function dijkstra() {
+
+}
+
+function astar() {
+
+}
+
+
+/* --------------------------- Utils ---------------------------- */
+
+/* Backtrack from Destination to Source */
+function backTrack(destNode) {
+    let path = [];
+    let curNode = destNode;
+    while (curNode.parent !== null) {
+        curNode = curNode.parent;
+        path.push(curNode.state);
+    }
+
+    return path.reverse();
+}
+
+
+/* Animate the search */
+function animateSearch(idx) {
+    const id = `${idx[0]}_${idx[1]}`;
+    const box = document.getElementById(id).children[0];
+    if (box) {
+        box.style.animationPlayState = 'running';
+    }
+}
+
+
+/* True if arr2 is inside arr1 (arr1 has index in arr1) */
+function exists(arr1, arr2) {
+    const arr1_str = JSON.stringify(arr1);
+    const arr2_str = JSON.stringify(arr2);
+
+    if (arr1_str.indexOf(arr2_str) != -1) {
+        return true;
+    }
+    return false;
+}
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+export { dfs, bfs, greedy, dijkstra, astar };
--- app.js
+++ app.js
@@ -1,32 +1,14 @@
 /* JavaScript for Pathfinding Visualizer */
 
+import { dfs, bfs, greedy, dijkstra, astar } from './algorithms.js';
+
 const TOTAL_ROW = 20;
 const TOTAL_COL = 50;
 const SRC = [9, 5];
 const DEST = [9, 44];
 var ALGORITHM = '';
+var neighbors = {};
 var walls = [];
-
-const fnMap = {
-    'DFS': dfs,
-    'BFS': bfs,
-    'Greedy': greedy,
-    'Dijkstra\'s': dijkstra,
-    'A*': astar
-};
-
-// Returns a Promise that resolves after "ms" Milliseconds
-const timer = ms => new Promise(res => setTimeout(res, ms));
-
-/* Search Algo Node */
-class Node {
-    constructor(state, parent) {
-        this.state = state;
-        this.parent = parent;
-    }
-}
-
-/* ----------------------------- Interactions -------------------------- */
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -48,6 +30,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    // Pre-process data
+    preprocess();
+
     // Event Listeners for Visualize Button
     document.querySelector('#visualize').addEventListener('click', selectAlgorithm);
 
@@ -61,6 +46,8 @@ document.addEventListener('DOMContentLoaded', function () {
             document.querySelector('#message').innerHTML = `${ALGORITHM} Algorithm`
         });
     });
+
+    dfs();
 });
 
 
@@ -98,6 +85,41 @@ function clear() {
 
     // Clear Message
     document.querySelector('#message').innerHTML = 'Click on Grid to Add Wall';
+}
+
+
+/* Pre-Process Neighbors Data
+For each cell with index (i, j):
+neighbors[[i, j]]: [[i - 1,j], [i + 1,j], ...]]
+*/
+function preprocess() {
+    for (let i = 0; i < TOTAL_ROW; i++) {
+        for (let j = 0; j < TOTAL_COL; j++) {
+
+            // Initialize an array for each cell
+            neighbors[[i, j]] = [];
+
+            // If not the last row, append cell with (row + 1)
+            if (i < TOTAL_ROW - 1) {
+                neighbors[[i, j]].push([i + 1, j]);
+            }
+
+            // If not the first col, append cell with (col - 1)
+            if (j > 0) {
+                neighbors[[i, j]].push([i, j - 1]);
+            }
+
+            // If not the last col, append col with (col + 1)
+            if (j < TOTAL_COL - 1) {
+                neighbors[[i, j]].push([i, j + 1]);
+            }
+
+            // If not the first row, append cell with (row - 1) 
+            if (i > 0) {
+                neighbors[[i, j]].push([i - 1, j]);
+            }
+        }
+    }
 }
 
 
@@ -157,129 +179,4 @@ function tableCreate() {
     grid.append(tbl);
 }
 
-
-/* -------------------------- Algorithms ----------------------------- */
-
-async function dfs() {
-
-    // Store all nodes to be traversed
-    let stackFrontier = [];
-    stackFrontier.push(new Node(SRC, null));
-
-    // Store all searched nodes to prevent infinite search
-    let searched = [];
-
-    // Traverse until no node can be found
-    while (stackFrontier.length != 0) {
-
-        // Search the next node
-        let curNode = stackFrontier.pop();
-        searched.push(curNode.state);
-
-        // Check if current node is the destination
-        if (JSON.stringify(curNode.state) === JSON.stringify(DEST)) {
-            return backTrack(curNode);
-        }
-
-        animateSearch(curNode.state);
-
-        // Push all the neighbors of current node
-        findNeighbors(curNode.state, 'dfs').forEach(idx => {
-            if (!exists(searched, idx)) {
-                stackFrontier.push(new Node(idx, curNode));
-            }
-        })
-        await timer(50);
-    }
-
-    return null;
-}
-
-
-function bfs() {
-
-}
-
-
-function greedy() {
-
-}
-
-
-function dijkstra() {
-
-}
-
-
-function astar() {
-
-}
-
-
-/* --------------------------- Utils ---------------------------- */
-
-/* Backtrack from Destination to Source */
-function backTrack(destNode) {
-    let path = [];
-    let curNode = destNode;
-    while (curNode.parent !== null) {
-        curNode = curNode.parent;
-        path.push(curNode.state);
-    }
-
-    return path.reverse();
-}
-
-
-/* Get neighbors of a cell (BFS, DFS) */
-function findNeighbors(idx, algo_name) {
-    const row = idx[0];
-    const col = idx[1];
-    let neighbors = [];
-
-    if ((row - 1 >= 0) && !exists(walls, [`${row - 1}`, 'col'])) {
-        if (algo_name === 'dfs') {
-            neighbors.unshift([row - 1, col]);
-        } else {
-            neighbors.push([row - 1, col]);
-        }
-    }
-
-    if ((col + 1 < TOTAL_COL) && !exists(walls, ['row', `${col + 1}`])) {
-        if (algo_name === 'dfs') neighbors.unshift([row, col + 1]);
-        else neighbors.push([row, col + 1]);
-    }
-
-    if ((row + 1 < TOTAL_ROW) && !exists(walls, [`${row + 1}`, 'col'])) {
-        if (algo_name === 'dfs') neighbors.unshift([row + 1, col]);
-        else neighbors.push([row + 1, col]);
-    }
-
-    if ((col - 1 >= 0) && !exists(walls, ['row', `${col - 1}`])) {
-        if (algo_name === 'dfs') neighbors.unshift([row, col - 1]);
-        else neighbors.push([row, col - 1]);
-    }
-
-    return neighbors;
-}
-
-/* Animate the search */
-function animateSearch(idx) {
-    const id = `${idx[0]}_${idx[1]}`;
-    const box = document.getElementById(id).children[0];
-    if (box) {
-        box.style.animationPlayState = 'running';
-    }
-}
-
-
-/* True if arr2 is inside arr1 (arr1 has index in arr1) */
-function exists(arr1, arr2) {
-    const arr1_str = JSON.stringify(arr1);
-    const arr2_str = JSON.stringify(arr2);
-
-    if (arr1_str.indexOf(arr2_str) != -1) {
-        return true;
-    }
-    return false;
-}
+export { SRC, DEST, neighbors };
